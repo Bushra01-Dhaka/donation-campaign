0c550c43dee3691e532b152f49053dec246acee2
Bushra01-Dhaka <humayraanjum87@gmail.com>
2023-09-25 10:44:51 +0600

    Added banner component

--- src/Components/Header/Banner/Banner.jsx
+++ src/Components/Header/Banner/Banner.jsx
@@ -1,10 +1,16 @@
 import './Banner.css'
 const Banner = () => {
     return (
-        <div className="banner-container ">
-            <div className=''>
-              
+        <div className="banner-container flex justify-center items-center ">
+            <div>
+                <h1 className='text-3xl md:text-4xl lg:text-6xl font-bold text-center text-[#0B0B0B] mb-10'>I Grow By Helping People In Need</h1>
+                <div className='flex justify-center items-center'>
+                    <input className='p-3 md:p-5 md:w-[450px]  rounded shadow-md border-none outline-red-500' placeholder='Search here' type="text" />
+                    <button className='btn btn-md md:btn-lg h-[50px] md:h-[50px]  border-none text-sm text-white  bg-[#FF444A] hover:bg-red-500 rounded'>Search</button>
+                </div>
+
             </div>
+
         </div>
     );
 };
--- src/Components/Header/NavBar.jsx
+++ src/Components/Header/NavBar.jsx
@@ -2,13 +2,13 @@ import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <div className="py-5 px-5 bg-transparent">
-            <nav className="flex flex-cols md:flex-row justify-between items-center">
+        <div className="py-5 px-5 bg-white">
+            <nav className="flex flex-col md:flex-row justify-between items-center">
                 <div>
                     <img className="h-[72px]" src="./images/Logo.png" alt="" />
                 </div>
                 <div>
-                    <ul className="flex justify-center items-center gap-4 font-semibold text-[#0B0B0B]">
+                    <ul className="flex justify-center items-center my-4 md:my-0 gap-4 font-semibold text-[#0B0B0B]">
                         <NavLink
                             to="/"
                             className={({ isActive, isPending }) =>
